002a6bae7e0eda0215a3534681773331b4d0d834
MangloVedic <54463145+RAVIRAUSHANSINGH@users.noreply.github.com>
2022-01-16 17:48:31 +0530

    Update Day 2: Operators.cpp

--- Day 2: Operators.cpp
+++ Day 2: Operators.cpp
@@ -3,5 +3,5 @@ void solve(double meal_cost, int tip_percent, int tax_percent) {
                 a= (meal_cost * tip_percent)/100;
                 b= (meal_cost * tax_percent)/100;
                 d = meal_cost+a+b;
-                cout<<round(d); /* round used for round figure point value
+                cout<<round(d); /* round used for round figure point value */
 }
