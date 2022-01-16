28bd5e085cdbbfdbd87afb2ab924bb5d95d9b79a
MangloVedic <raviraushanlyf@gmail.com>
2022-01-16 17:38:50 +0530

    Hacker Rank Solution in CPP

---
+++ Day 1: Data Types.cpp
@@ -0,0 +1,11 @@
+     int a;
+     double b;
+     string str;
+     cin>>a>>b;
+     getline(cin>>ws,str); /*ws for white space a/c to question */
+     double c;
+     cout<<a+i<<endl;
+     cout<<fixed<<setprecision(1)<<(b+d)<<endl;
+     cout<<s;
+     cout<<str;
+     // cout<<s+str;  we can this also
