419a5d3d88aa280c1c617c65f172013f5f09ca9b
MangloVedic <raviraushanlyf@gmail.com>
2022-01-18 21:05:25 +0530

    CPP

---
+++ Day 3: Intro to Conditional Statements.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+        if(n%2!=0){
+            cout<<"Weird"<<endl;
+        }else if(n>=2 and n<=5){
+            cout<<"Not Weird"<<endl;
+        }else if (n>=6 and n<=20){
+            cout<<"Weird"<<endl;
+        }else{
+            cout<<"Not Weird"<<endl;
+        }
+        
+    
+
+    return 0;
+}
