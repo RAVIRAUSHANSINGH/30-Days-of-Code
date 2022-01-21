1540d257c13d3372fa360da8f63248dfa1ae7e82
MangloVedic <raviraushanlyf@gmail.com>
2022-01-22 00:01:03 +0530

    Cpp

---
+++ Day 5: Loops.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    string str;
+    for(int i=1;i<=10;i++){
+        int a=t*i;
+        cout<<t<<" x "<<i<<" = "<<a<<endl;
+    }
+    return 0;
+}
