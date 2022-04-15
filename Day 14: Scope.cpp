a1b4f21522490c891ec988f231960e6ffa5887e8
MangloVedic <54463145+RAVIRAUSHANSINGH@users.noreply.github.com>
2022-04-15 14:19:35 +0530

    Create Day 14: Scope.cpp

---
+++ Day 14: Scope.cpp
@@ -0,0 +1,10 @@
+
+   Difference(vector<int> arr){
+        elements = arr;
+        sort(elements.begin(), elements.end());
+    }
+
+    void computeDifference(){
+        
+        maximumDifference = abs(elements[elements.size()-1] - elements[0]);
+    }
