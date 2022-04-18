e2783c8dbd132979ffd05c2b3ec2246117be037c
MangloVedic <54463145+RAVIRAUSHANSINGH@users.noreply.github.com>
2022-04-19 04:04:42 +0530

    Create Day 17: More Exceptions.cpp

---
+++ Day 17: More Exceptions.cpp
@@ -0,0 +1,10 @@
+class Calculator {
+    public:
+        int power(int n, int p){
+            if(n < 0 || p < 0){
+                throw invalid_argument("n and p should be non-negative"); 
+            }
+            
+            return pow(n, p);
+        }
+};
